5e67c25f8dc0752143d03fb12375d79232b7cc2a
FaviohuamanVG <favio.huaman@vallegrande.edu.pe>
2025-09-11 21:33:48 -0500

    feat(user) update views of users

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,7 @@
 export { default as studentService } from './student.service';
 export { default as classroomStudentService } from './classroom-student.service';
 export { gradeService } from './gradeService';
-export { notificationService } from './notificationService'; 
+export { notificationService } from './notificationService';
+export { default as adminUserService } from './adminDirectorService/adminUserService';
+export { default as directorUserService } from './adminDirectorService/directorUserService';
+export { userInstitutionService } from './adminDirectorService/userInstitutionService';
